f794a7345674a3167de4eabc863f1e8f7898e186
hota1024 <hotalog@hota1024.com>
2020-05-27 21:25:12 +0900

    🏷️ Update src/classes/index.ts

--- src/classes/index.ts
+++ src/classes/index.ts
@@ -1,5 +1,6 @@
 export * from './Evaluator'
 export * from './EvaluatorError'
+export * from './EvaluatorSyntaxError'
 export * from './Lexer'
 export * from './LexerError'
 export * from './Location'
