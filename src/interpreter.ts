ccfbe08faf80660db6e855ca53b4b55fd71665df
hota1024 <hotalog@hota1024.com>
2020-05-27 12:52:19 +0900

    🏷️ Add interpreter.ts

---
+++ src/interpreter.ts
@@ -0,0 +1,29 @@
+import * as readline from 'readline'
+import { Lexer, Parser, Evaluator } from './classes'
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const lexer = new Lexer()
+const parser = new Parser()
+const evaluator = new Evaluator()
+
+const input = () =>
+  rl.question('-> ', (line) => {
+    if (line === '.exit') {
+      return
+    }
+
+    const tokens = lexer.analyze(line)
+    const ast = parser.parse(tokens)
+    const result = evaluator.evaluate(ast)
+
+    console.log(`<- ${result}`)
+
+    input()
+  })
+
+console.log('To exit, press ^C again or ^D or type .exit')
+input()
