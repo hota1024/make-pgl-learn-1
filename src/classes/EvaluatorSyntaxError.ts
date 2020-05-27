a061e40aba4382f8cf0e9b412f58e36bd6891bb9
hota1024 <hotalog@hota1024.com>
2020-05-27 21:18:27 +0900

    🏷️ Add EvaluatorSyntaxError class

---
+++ src/classes/EvaluatorSyntaxError.ts
@@ -0,0 +1,8 @@
+import { EvaluatorError } from './EvaluatorError'
+
+/*
+ * EvaluatorReferenceError class.
+ */
+export class EvaluatorReferenceError extends EvaluatorError {
+  name = 'EvaluatorReferenceError'
+}
