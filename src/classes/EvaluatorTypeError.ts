c88747dc3f6068fda6e62c92c1cb1d7154271c3c
hota1024 <hotalog@hota1024.com>
2020-05-27 21:28:30 +0900

    🏷️ Add EvaluatorTypeError calss

---
+++ src/classes/EvaluatorTypeError.ts
@@ -0,0 +1,8 @@
+import { EvaluatorError } from './EvaluatorError'
+
+/*
+ * EvaluatorTypeError class.
+ */
+export class EvaluatorTypeError extends EvaluatorError {
+  name = 'EvaluatorTypeError'
+}
