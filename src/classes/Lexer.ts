bce0e0c49f559c7c239cf6748ca32b04f17ec58d
hota1024 <hotalog@hota1024.com>
2020-05-28 11:01:00 +0900

    🏷️ Update Lexer class

--- src/classes/Lexer.ts
+++ src/classes/Lexer.ts
@@ -11,6 +11,25 @@ export class Lexer implements ILexer {
     const tokens: Token[] = []
     let pos = 0
 
+    const match = (...expects: string[]) => {
+      for (const expect of expects) {
+        let match = true
+
+        for (let i = 0; i < expect.length; ++i) {
+          if (expect[i] !== source[pos + i]) {
+            match = false
+            break
+          }
+        }
+
+        if (match) {
+          return expect
+        }
+      }
+
+      return false
+    }
+
     while (pos < source.length) {
       if (/[0-9]|\./.test(source[pos])) {
         const start = pos
@@ -36,10 +55,10 @@ export class Lexer implements ILexer {
             new Location(start, pos)
           )
         )
-      } else if (/[a-zA-Z]/.test(source[pos])) {
+      } else if (/[a-zA-Z]|_/.test(source[pos])) {
         const start = pos
 
-        while (pos < source.length && /[a-zA-Z0-9]/.test(source[pos])) {
+        while (pos < source.length && /[a-zA-Z0-9]|_/.test(source[pos])) {
           ++pos
         }
 
@@ -49,33 +68,68 @@ export class Lexer implements ILexer {
             new Location(start, pos)
           )
         )
-      } else if ('+-*/(),'.includes(source[pos])) {
-        let symbol: SymbolToken['symbol']
-
-        if (source[pos] === '+') {
-          symbol = 'plus'
-        } else if (source[pos] === '-') {
-          symbol = 'minus'
-        } else if (source[pos] === '*') {
-          symbol = 'asterisk'
-        } else if (source[pos] === '/') {
-          symbol = 'slash'
-        } else if (source[pos] === '(') {
-          symbol = 'left_parenthesis'
-        } else if (source[pos] === ')') {
-          symbol = 'right_parenthesis'
-        } else if (source[pos] === ',') {
-          symbol = 'comma'
-        }
-
-        tokens.push(this.makeSymbol(symbol, new Location(pos, pos + 1)))
-        ++pos
-      } else if (source[pos] === ' ') {
-        while (source[pos] === ' ') {
-          ++pos
-        }
       } else {
-        throw new LexerError('illegal character', new Location(pos, pos + 1))
+        const symbol = match(
+          '+=',
+          '-=',
+          '*=',
+          '/=',
+          '%=',
+          '==',
+          '!=',
+          '<=',
+          '>='
+        )
+        if (symbol) {
+          const map: {
+            [K: string]: SymbolToken['symbol']
+          } = {
+            '+=': 'plus_equal',
+            '-=': 'minus_equal',
+            '*=': 'asterisk_equal',
+            '/=': 'slash_equal',
+            '%=': 'percent_equal',
+            '==': 'equal_equal',
+            '!=': 'exclamation_equal',
+            '<=': 'less_equal_than',
+            '>=': 'greeter_equal_than',
+          }
+
+          tokens.push(
+            this.makeSymbol(map[symbol], new Location(pos, symbol.length))
+          )
+
+          pos += symbol.length
+        } else if ('+-*/%!(),;=<>'.includes(source[pos])) {
+          const map: {
+            [K: string]: SymbolToken['symbol']
+          } = {
+            '+': 'plus',
+            '-': 'minus',
+            '*': 'asterisk',
+            '/': 'slash',
+            '%': 'percent',
+            '!': 'exclamation',
+            '(': 'left_parenthesis',
+            ')': 'right_parenthesis',
+            ',': 'comma',
+            ';': 'semicolon',
+            '=': 'equal',
+            '<': 'less_than',
+            '>': 'greeter_than',
+          }
+
+          tokens.push(
+            this.makeSymbol(map[source[pos]], new Location(pos, pos + 1))
+          )
+          ++pos
+        } else if (source[pos] === ' ') {
+          while (source[pos] === ' ') {
+            ++pos
+          }
+        } else {
+          throw new LexerError('illegal character', new Location(pos, pos + 1))
+        }
       }
     }
 
